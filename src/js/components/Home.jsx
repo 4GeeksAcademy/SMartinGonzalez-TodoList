3f706f4e9da519e6fcf38fddc4ef0011d2cb2d61
Santiago <126203646+SMartinGonzalez@users.noreply.github.com>
2025-03-12 20:31:59 +0000

    commit

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,62 @@
-import React from "react";
+import React, {useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [inputValue, setInputValue] = useState("");
+	const [todos, setTodos] = useState([]);
+	const [hoveredTask, setHoveredTask] = useState(null);
+  
 	return (
-		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+	  <div className="container">
+		<h1>Todos</h1>
+		<ul>
+		  <li>
+			<input
+			  type="text"
+			  className="form-control"
+			  onChange={(e) => setInputValue(e.target.value)}
+			  value={inputValue}
+			  onKeyDown={(e) => {
+				if (e.key === "Enter" && inputValue.trim() !== "") {
+				  setTodos([...todos, inputValue]);
+				  setInputValue("");
+				}
+			  }}
+			  placeholder="What do you need to do?"
+			/>
+		  </li>
+		  {todos.map((item, index) => (
+			<li
+			  key={index}
+			  onMouseEnter={() => setHoveredTask(index)}
+			  onMouseLeave={() => setHoveredTask(null)}
+			>
+			  {item}{" "}
+			  {hoveredTask === index && (
+				<i
+				  className="fa-solid fa-x"
+				  onClick={() =>
+					setTodos(todos.filter((_, currentIndex) => index !== currentIndex))
+				  }
+				></i>
+			  )}
+			</li>
+		  ))}
+		</ul>
+		<div>
+        {todos.length === 0 ? (
+          <span>
+            No hay tareas, añadir tareas
+          </span>
+        ) : (
+          `${todos.length} task/s left`
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
